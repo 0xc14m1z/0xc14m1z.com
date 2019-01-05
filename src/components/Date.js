249d4616907b49116d07177a44c9e6012dadbb39
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-05 16:42:35 -0600

    Draft Date component

---
+++ src/components/Date.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Date = styled.p `
+  margin: 0px;
+  font-size: 0.9em;
+  color: #888888;
+`
+
+export default React.memo(Date)
