483e14e2246bf34994566d2f9331c77401c8f9a5
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-05 15:34:12 -0600

    Draft H1 component

---
+++ src/components/H1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const H1 = styled.h1 `
+  text-align: center;
+  color: #930EDF;
+  font-family: 'Playfair Display', serif;
+`
+
+export default React.memo(H1)
