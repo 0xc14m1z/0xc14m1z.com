d9da5dd1b457ff386f02d354dceaa42b3d8577e7
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-05 17:06:43 -0600

    Removed font-family because added directly to the body element

--- src/components/H1.js
+++ src/components/H1.js
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 const H1 = styled.h1 `
   text-align: center;
   color: #930EDF;
-  font-family: 'Playfair Display', serif;
 `
 
 export default React.memo(H1)
