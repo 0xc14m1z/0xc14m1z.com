e2a31c8893a5ed1b790552568ca26a279e767e76
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-05 17:08:31 -0600

    Add Header component

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,16 +2,23 @@ import React from 'react'
 import t from 'prop-types'
 import styled from 'styled-components'
 
+import Header from 'components/Header'
+
 const Container = styled.div `
   max-width: 768px;
-  box-sizing: border-box;
   margin: auto;
+`
+
+const Main = styled.main `
   padding: 0px 20px;
 `
 
 const Page = ({ children }) => (
   <Container>
-    { children }
+    <Header />
+    <Main>
+      { children }
+    </Main>
   </Container>
 )
 
