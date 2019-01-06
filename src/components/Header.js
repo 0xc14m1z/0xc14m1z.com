aaa734aa5ee28426b821feaa372d82086883b378
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-06 16:18:15 -0500

    Add link to the About page to the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import IfNotHome from 'components/IfNotHome'
 import Link from 'components/Link'
+import Icon from 'components/Icon'
 
 const Container = styled.header `
   position: sticky;
   top: 0px;
   left: 0px;
   right: 0px;
+  height: 40px;
   margin-bottom: 30px;
   display: flex;
-  justify-content: center;
-  align-items: space-between;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 20px;
+  box-sizing: border-box;
   border-bottom: 1px solid #DDDDDD;
   background: white;
 `
 
 const Title = styled.span `
-  padding: 10px 0px;
   color: black;
   font-size: 1em;
   font-weight: bold;
 `
 
-const Back = () => (
-  <Link to="/">Home</Link>
+const Home = () => (
+  <Link to="/"><Icon name="home" /></Link>
+)
+
+const About = () => (
+  <Link to="/about"><Icon name="info-circle" /></Link>
 )
 
 const Header = () => (
   <Container>
-    <IfNotHome component={ Back } />
+    <Home />
     <Title>0xc14m1z notes</Title>
+    <About />
   </Container>
 )
 
