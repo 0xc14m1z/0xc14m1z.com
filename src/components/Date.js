f5ae7c1fa9764ac413b8e2b1574703b65ec4e53c
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-06 00:59:03 -0500

    Add date formatting

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Date = styled.p `
+import FormattedDate from 'components/FormattedDate'
+
+const Container = styled.p `
   margin: 0px;
   font-size: 0.9em;
   color: #888888;
 `
+
+const Date = ({ date }) => (
+  <Container>
+    <FormattedDate date={ date } />
+  </Container>
+)
 
 export default React.memo(Date)
