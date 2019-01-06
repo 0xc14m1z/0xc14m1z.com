72ef1d6ef40b6c5bfea5c32e804e9979a3c10a2a
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-06 16:20:50 -0500

    Draft test articles

---
+++ src/articles/2019/01/02/how-would-i-build-a-micro-version-of-react-part-1.md
@@ -0,0 +1,14 @@
+This little experimental project has come to my mind during a vacation in Costa
+Rica. I was a bit struggling without almost any internet connection (accordingly
+ to fast.com it was of about 800 bytes per second), and during a very bad
+ weather moment I asked myself:
+
+> how would I have written something like React?
+
+I felt quite comfortable answering myself with general architecture and steps
+but I also wanted to try everything out. I have no idea at all whether this
+experiment will ever finish, but I am sure it will be very educational.
+
+To add a bit of challenge in this (just because I have no idea of what I am
+  doing), I’d like to implement everything in a "as munch functional" possible
+  way.
---
+++ src/articles/index.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    slug: '2019/01/02/how-would-i-build-a-micro-version-of-react-part-1',
+    date: new Date(2019, 1, 2),
+    title: 'How would I build a micro version of React? - Part 1',
+    intro: 'The first of a series of articles in which I try to build a small version of React, trying to understand how it may work.',
+    content: require('./2019/01/02/how-would-i-build-a-micro-version-of-react-part-1.md'),
+    tags: ['javascript', 'how to', 'react', 'experiment']
+  },
+  {
+    slug: '2019/01/03/how-would-i-build-a-micro-version-of-react-part-2',
+    date: new Date(2019, 1, 3),
+    title: 'How would I build a micro version of React? - Part 2',
+    intro: 'Second part of the series, lay out the general idea behind a framework like React.',
+    content: require('./2019/01/03/how-would-i-build-a-micro-version-of-react-part-2.md'),
+    tags: ['javascript', 'how to', 'react']
+  }
+]
