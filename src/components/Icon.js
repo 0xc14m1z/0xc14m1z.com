cdaac63e72dad77f8169b37af216525118e50458
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-07 22:17:30 +0100

    Add a couple more icons

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,18 +3,31 @@ import t from 'prop-types'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-import { faHome, faInfoCircle } from '@fortawesome/pro-light-svg-icons'
+import {
+  faHome,
+  faInfoCircle,
+  faEmptySet,
+  faFrownOpen
+} from '@fortawesome/pro-light-svg-icons'
 
 library.add(
-  faHome, faInfoCircle
+  faHome,
+  faInfoCircle,
+  faEmptySet,
+  faFrownOpen
 )
 
-const Icon = ({ name }) => (
-  <FontAwesomeIcon icon={['fal', name]} />
+const Icon = ({ name, ...props }) => (
+  <FontAwesomeIcon icon={['fal', name]} { ...props } />
 )
 
 Icon.propTypes = {
-  name: t.string.isRequired
+  name: t.oneOf([
+    'home',
+    'info-circle',
+    'empty-set',
+    'frown-open'
+  ]).isRequired
 }
 
 export default React.memo(Icon)
