feac9e2d33f205d44585951499aa45eb22da4f56
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-07 20:10:37 +0100

    Draft Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import photo from 'assets/images/photo.jpg'
+
+import Link from 'components/Link'
+import BaseH1 from 'components/H1'
+import BaseH2 from 'components/H2'
+
+const Container = styled.div `
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &:after {
+    display: block;
+    margin: 20px 0px 50px 0px;
+    content: "•";
+    color: #DDDDDD;
+    text-align: center;
+  }
+`
+
+const Photo = styled.img `
+  width: 72px;
+  height: 72px;
+  border-radius: 1000px;
+  margin-bottom: 20px;
+`
+
+const H1 = styled(BaseH1) `
+  margin: 0px;
+  font-size: 1em;
+`
+
+const H2 = styled(BaseH2) `
+  margin: 0px;
+  font-size: 1em;
+  font-weight: normal;
+  color: black;
+`
+
+const Hero = () => (
+  <Container>
+    <Link to="/about"><Photo src={ photo } alt="Luca Ramundo" /></Link>
+    <H1>0xc14m1z notes</H1>
+    <H2>personal blog of Luca Ramundo</H2>
+  </Container>
+)
+
+export default React.memo(Hero)
