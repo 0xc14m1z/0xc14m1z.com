3671ff75647c55bbd43dba3f0b9c8a0e80569499
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-10 02:48:57 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import { Switch, Route } from 'react-router'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import * as Pages from 'pages'
+
 const App = () => (
-  <div>
-    0xc14m1z
-  </div>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={ Pages.HomePage } />
+      <Route exact path="/about" component={ Pages.AboutPage } />
+
+      <Route path="/:slug*" component={ Pages.ArticlePage } />
+
+      <Route component={ Pages.NotFoundPage } />
+    </Switch>
+  </Router>
 )
 
 export default React.memo(App)
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as AboutPage } from './AboutPage'
+export { default as ArticlePage } from './ArticlePage'
+export { default as HomePage } from './HomePage'
+export { default as NotFoundPage } from './NotFoundPage'
