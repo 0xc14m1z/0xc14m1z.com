ed4e1faf82e3b251f44098c2e6dcc6e7993ab70d
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-10 14:02:52 +0100

    Add draft to articles list

---
+++ src/articles/2018/09/building-a-react-paginator-with-a-predefined-number-of-visible-pages.md
@@ -0,0 +1,9 @@
+I am working on the React backoffice application rewrite for an Opera website and, as common, I need to paginate the results I get from the backend.
+
+Let's start with the result:
+
+[ paginator screenshot here ]
+
+My requirements are quite standard:
+
+> we need show a "window" of N pages, some before and some after the current one, which is supposed to be at the center.
--- src/articles/index.js
+++ src/articles/index.js
@@ -3,7 +3,7 @@ import moment from 'moment'
 const list = [
   {
     slug: '2019/01/02/building-a-micro-version-of-react-from-scratch',
-    date: moment('2019-01-02'),
+    date: moment('2019-01-02').toDate(),
     title: 'Building a micro version of React from scratch',
     intro: 'The first of a series of articles in which I try to build a small version of React, trying to understand how it may work.',
     content: require('./2019/01/building-a-micro-version-of-react-from-scratch.md'),
@@ -11,7 +11,7 @@ const list = [
   },
   {
     slug: '2018/12/13/how-to-use-font-awesome-pro-with-react',
-    date: moment('2018-12-13'),
+    date: moment('2018-12-13').toDate(),
     title: 'How to use FontAwesome Pro with React',
     intro: 'Step by step from environment setup to dedicated component.',
     content: require('./2018/12/how-to-use-font-awesome-pro-with-react.md'),
@@ -19,15 +19,23 @@ const list = [
   },
   {
     slug: '2018/08/10/building-a-functional-sql-generator-from-plain-old-javascript-objects',
-    date: moment('2018-10-09'),
+    date: moment('2018-10-09').toDate(),
     title: 'Building a functional SQL generator from Plain Old Javascript Objects',
     intro: 'Is it possible to generate SQL queries from POJOs? Would it be convinient and easy? Let\'s find out.',
     content: require('./2018/10/building-a-functional-sql-generator-from-plain-old-javascript-objects.md'),
     tags: ['javascript', 'functional programming', 'sql', 'experiment']
   },
   {
+    slug: '2018/09/08/building-a-react-paginator-with-a-predefined-number-of-visible-pages',
+    date: moment('2018-09-08').toDate(),
+    title: 'Building a React paginator with predefined number of visible pages',
+    intro: 'A surprisingly interesting problem to solve.',
+    content: require('./2018/09/building-a-react-paginator-with-a-predefined-number-of-visible-pages.md'),
+    tags: ['javascript', 'how to', 'react']
+  },
+  {
     slug: '2018/08/31/why-array-sort-in-javascript-doesnt-sort-numbers',
-    date: moment('2018-08-31'),
+    date: moment('2018-08-31').toDate(),
     title: 'Why array sort() in Javascript doesn’t sort numbers?',
     intro: 'Understand an at-first weird Javascript behaviour and "sort it out".',
     content: require('./2018/08/why-array-sort-in-javascript-doesnt-sort-numbers.md'),
