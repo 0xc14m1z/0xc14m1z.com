d6afd869dbef5678bef05c4a806c08c3dff399d4
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-10 19:19:51 +0100

    Use moment instead of raw date objects to define articles dates

--- src/articles/index.js
+++ src/articles/index.js
@@ -1,7 +1,9 @@
+import moment from 'moment'
+
 const list = [
   {
     slug: '2019/01/02/building-a-micro-version-of-react-from-scratch',
-    date: new Date(2019, 0, 2),
+    date: moment('2019-01-02').toDate(),
     title: 'Building a micro version of React from scratch',
     intro: 'The first of a series of articles in which I try to build a small version of React, trying to understand how it may work.',
     content: require('./2019/01/building-a-micro-version-of-react-from-scratch.md'),
@@ -9,7 +11,7 @@ const list = [
   },
   {
     slug: '2018/12/13/how-to-use-font-awesome-pro-with-react',
-    date: new Date(2018, 11, 13),
+    date: moment('2018-12-13').toDate(),
     title: 'How to use FontAwesome Pro with React',
     intro: 'Step by step from environment setup to dedicated component.',
     content: require('./2018/12/how-to-use-font-awesome-pro-with-react.md'),
@@ -17,7 +19,7 @@ const list = [
   },
   {
     slug: '2018/08/31/why-array-sort-in-javascript-doesnt-sort-numbers',
-    date: new Date(2018, 11, 13),
+    date: moment('2018-08-31').toDate(),
     title: 'Why array sort() in Javascript doesn’t sort numbers?',
     intro: 'Understand an at-first weird Javascript behaviour and "sort it out".',
     content: require('./2018/08/why-array-sort-in-javascript-doesnt-sort-numbers.md'),
