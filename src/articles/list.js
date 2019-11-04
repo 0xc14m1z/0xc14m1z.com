09e6d64d449cb629b3b22424722605d8d48bde5c
Luca Ramundo <0xc14m1z@gmail.com>
2019-11-04 21:25:10 +0100

    Move articles list to a dedicated file

---
+++ src/articles/list.js
@@ -0,0 +1,26 @@
+const list = [
+  {
+    slug: 'articles/building-a-micro-version-of-react-from-scratch',
+    title: 'Building a micro version of React from scratch',
+    intro: 'The first of a series of articles in which I try to build a small version of React, trying to understand how it may work.',
+    content: require('./building-a-micro-version-of-react-from-scratch.md'),
+    tags: ['javascript', 'how to', 'react', 'experiment'],
+    isWorkInProgress: true
+  },
+  {
+    slug: 'articles/how-to-use-font-awesome-pro-with-react',
+    title: 'How to use FontAwesome Pro with React',
+    intro: 'Step by step from environment setup to dedicated component.',
+    content: require('./how-to-use-font-awesome-pro-with-react.md'),
+    tags: ['javascript', 'how to', 'react', 'font awesome']
+  },
+  {
+    slug: 'articles/why-array-sort-in-javascript-doesnt-sort-numbers',
+    title: 'Why array sort() in Javascript doesn’t sort numbers?',
+    intro: 'Understand an at-first weird Javascript behaviour and "sort it out".',
+    content: require('./why-array-sort-in-javascript-doesnt-sort-numbers.md'),
+    tags: ['javascript', 'why']
+  }
+]
+
+export default list
