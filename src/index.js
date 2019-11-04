a146a7d18a0f30791895050c5925713cac0a2bd0
0xc14m1z <0xc14m1z@gmail.com>
2019-11-04 17:24:57 +0100

    Temporary disable service worker

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import 'typeface-nunito'
 import 'typeface-source-code-pro'
 
 import Application from './Application'
-import * as serviceWorker from './serviceWorker'
+// import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<Application />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// serviceWorker.unregister()
