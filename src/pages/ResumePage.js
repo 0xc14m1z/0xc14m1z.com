907b5eddaa87d427349ddefaaedcc17555ab16d7
Luca Ramundo <0xc14m1z@gmail.com>
2019-11-04 19:59:22 +0100

    Update resume page introduction

--- src/pages/ResumePage.js
+++ src/pages/ResumePage.js
@@ -7,25 +7,26 @@ const ResumePage = () => (
     <H1>Luca Ramundo</H1>
 
     <p>
-      I'm an experienced Frontend Developer with more than ten years of full
-      stack projects behind me as a freelance.
+      I'm an experienced frontend developer with more than years of full stack
+      projects behind me as a freelance.
     </p>
     <p>
-      I worked with lots of different languages and frameworks over the course
-      of the years, but I am settling with the React ecosystem since late 2015.
-      I have extensive knowledge of Javascript/ES6, React, Redux and related
-      technologies.
+      I worked with lots of different languages and frameworks, but I fall in
+      love with React ecosystem since late 2015. I am familiar with
+      Javascript/ES6, React, Redux and related technologies.
     </p>
     <p>
       Keen to functional programming, my code is clean, understandable,
       maintainable and robust.
     </p>
     <p>
-      I'm eager to constantly improve my skills using all the learning platforms
-      available. Currently I am interested in deepen my Javascript knowledge and
-      studying functional programming languages like Elm for the frontend and
-      Elixir for the backend.
+      I'm eager to constantly improve my skills using all learning platforms
+      available. <br />
+      Currently I am interested in deepen my Javascript knowledge, getting into
+      Typescript and studying functional programming languages like Elm for the
+      frontend and Elixir for the backend.
     </p>
+
   </Page>
 )
 
